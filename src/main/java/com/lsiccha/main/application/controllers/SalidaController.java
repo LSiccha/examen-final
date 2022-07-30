e7f56591052b9d0fc536f612bfec7a279bb94271
LSiccha <gabrielluis_2001@hotmail.com>
2022-07-30 08:17:18 -0500

    port xd

--- src/main/java/com/lsiccha/main/application/controllers/SalidaController.java
+++ src/main/java/com/lsiccha/main/application/controllers/SalidaController.java
@@ -1,4 +1,25 @@
 package com.lsiccha.main.application.controllers;
 
+import com.lsiccha.main.domain.model.Salida;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public interface SalidaController {
+
+    @PostMapping
+    ResponseEntity<Salida> create(@RequestBody Salida salida);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Salida> getById(@PathVariable("id") Integer id);
+
+    @GetMapping()
+    ResponseEntity<List<Salida>> getAll();
+
+    @PutMapping
+    ResponseEntity<Salida> update(@RequestBody Salida salida);
+
+    @DeleteMapping(path = "/{id}")
+    ResponseEntity<Void> delete(@PathVariable("id") Integer id);
 }
--- src/main/java/com/lsiccha/main/application/controllers/impl/SalidaControllerImpl.java
+++ src/main/java/com/lsiccha/main/application/controllers/impl/SalidaControllerImpl.java
@@ -1,19 +1,56 @@
 package com.lsiccha.main.application.controllers.impl;
 
+import com.lsiccha.main.application.controllers.SalidaController;
+import com.lsiccha.main.domain.model.Salida;
+import com.lsiccha.main.domain.services.SalidaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("")
-public class SalidaControllerImpl {
+public class SalidaControllerImpl implements SalidaController {
 
-    @GetMapping
-    public ResponseEntity<String> hello(){
-        String message = "Hello Docker";
-        ResponseEntity<String> response = new ResponseEntity<>(message, HttpStatus.OK);
+    private final SalidaService salidaService;
+
+    SalidaControllerImpl(SalidaService salidaService){
+        this.salidaService = salidaService;
+    }
+    @Override
+    public ResponseEntity<Salida> create(Salida salida) {
+        Salida saved = this.salidaService.save(salida);
+        ResponseEntity<Salida> response = new ResponseEntity<>(saved, HttpStatus.CREATED);
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<Salida> getById(Integer id) {
+        Salida found = this.salidaService.getById(id);
+        ResponseEntity<Salida> response = new ResponseEntity<>(found, HttpStatus.OK);
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<List<Salida>> getAll() {
+        List<Salida> fromDb = this.salidaService.getAll();
+        ResponseEntity<List<Salida>> response = new ResponseEntity<>(fromDb, HttpStatus.OK);
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<Salida> update(Salida salida) {
+        Salida updated = this.salidaService.update(salida);
+        ResponseEntity<Salida> response = new ResponseEntity<>(updated, HttpStatus.OK);
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Integer id) {
+        this.salidaService.delete(id);
+        ResponseEntity<Void> response = new ResponseEntity<>(HttpStatus.CREATED);
         return response;
     }
 }
